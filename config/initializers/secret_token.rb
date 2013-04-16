ae0c82f379cf79eb3abc89430fc6dd722f3e2389
bryantzheng <bo.bozheng@gmail.com>
2013-04-16 22:56:46 +0800

    Add this for learning Ruby on Rails development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '63a4412b94053cc3f884377b11a1b013c9c2dc1bb29e7dd9d678bb72250cb0d1c0e924cb820362a95f3efa8e5c7910f3b89877ba1b6d0a8d18162a26ff540721'
