aa808fdf0aa9f22687f67b6a94f47b081a51b62e
bryantzheng <bo.bozheng@gmail.com>
2013-04-17 23:40:52 +0800

    Finished 11.1.2

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -46,11 +46,11 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart)}
+        format.html { redirect_to(store_url)}
         format.xml { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render action: "new" }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.xml { render xml: @line_item.errors, status: :unprocessable_entity }
       end
     end
   end
